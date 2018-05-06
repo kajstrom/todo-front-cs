a82f606eea6369a4e494546631385e3ccb71cb2d
Kaj Ström <kaj.strom@kapsi.fi>
2018-05-06 21:51:11 +0300

    Todo retrieval

--- src/todo_front_cs/core.cljs
+++ src/todo_front_cs/core.cljs
@@ -1,12 +1,19 @@
 (ns todo-front-cs.core
     (:require
-      [reagent.core :as r]))
+      [reagent.core :as r]
+      [ajax.core :as ajax]))
+
+(def todos (r/atom []))
+(defn get-todos
+  []
+  (ajax/GET "https://cvm3yhffx8.execute-api.eu-central-1.amazonaws.com/latest/todos"
+            {:handler #(reset! todos %)}))
 
 ;; -------------------------
 ;; Views
 
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+  [:div [:h2 "Welcome to TODO"]])
 
 ;; -------------------------
 ;; Initialize app
